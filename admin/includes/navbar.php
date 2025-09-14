7fc5cf9cb9f8e9c6aa2d2893da15c6ef60e9130e
jeni33 <168066781+jeni33@users.noreply.github.com>
2025-09-15 00:41:53 +0800

    Create navbar.php

---
+++ admin/includes/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark p-3" style="background-color: #008080;">
+  <a class="navbar-brand font-weight-bold" href="index.php">🛠 Admin Panel</a>
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" 
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="articles_from_students.php">📄 Pending Articles</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="articles_submitted.php">✅ Articles Submitted</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-danger font-weight-bold" href="login.php">🚪 Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
